73f2fe33896ac02171ab2c7921d0192e988f1b7b
gilroyneil <neil@osborntechnologies.com>
2015-05-14 20:39:56 +0100

    wewew

--- Extensions/Common.ps1
+++ Extensions/Common.ps1
@@ -117,8 +117,20 @@ function Get-TruncatedExponentialBackoffDelay([int]$PreviousBackoffDelay, [int]$
 # If the folder does not exists, then it is created.
 function Start-ScriptLog
 {
+    param
+    (
+        [String]$LogName
+    )
+
     $logDirectory = $env:SystemDrive + "\DeploymentLogs"
-    $logFilePath = $logDirectory + "\log.txt"
+    if (($LogName -ne $null) -or ($LogName -ne ""))
+    {
+        $logFilePath = $logDirectory + "\" + $LogName + ".txt"    
+    }
+    else
+    {    
+        $logFilePath = $logDirectory + "\log.txt"
+    }
 
     if(!(Test-Path -Path $logDirectory ))
     {
