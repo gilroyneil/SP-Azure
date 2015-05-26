ef847a214d68018ea5e37279e91dd5bdf504d35b
gilroyneil <neil@osborntechnologies.com>
2015-05-26 14:22:44 +0100

    ddd

--- Extensions/SetupSQL.ps1
+++ Extensions/SetupSQL.ps1
@@ -30,12 +30,12 @@ $GLOBAL_scriptExitCode = 0
 $DomainName = "osazure.com"
 $domainNetBiosName = "osazure"
 $DomainAdministratorUserName = "ngadmin"
-$DomainAdministratorPassword = "Start123"
+$DomainAdministratorPassword = "D1sabl3d281660"
 $ServiceUserName = "osazure2"
 $ServicePassword = "ss"
 
 
-Start-ScriptLog "Setup SQL"
+Start-ScriptLog
 
 
 
@@ -81,7 +81,7 @@ configuration SQLServer2014
 
             SourcePath = "e:\data\media"
             SourceFolder = "\SW_DVD9_NTRL_SQL_Svr_Std_Ent_Dev_BI_2014_English_FPP_OEM_X19-33828"
-            Features= "SQLENGINE,REPLICATION,FULLTEXT,CONN,BC,BOL,SSMS,ADV_SSMS"
+            Features= "SQLENGINE"
             InstanceName="SP"
             InstanceID="SP"
             SQLSysAdminAccounts="BUILTIN\ADMINISTRATORS" 
